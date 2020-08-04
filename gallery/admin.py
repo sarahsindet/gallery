6ec016810a7e8cd5b2c49f3a2dc7c6c14324f918
sarahsindet <sarahsindet@gmail.com>
2020-08-04 09:50:40 +0300

    imported admin for image models

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Image, Category, Location
+
+# Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    filter_horizontal =('Category')
+
+admin.site.register(Image)
+admin.site.register(Category)
+admin.site.register(Location)
